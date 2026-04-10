b43d0d45533795c7401a8d8522f5c79624833a74
3plus1wheels <3plus1wheels@gmail.com>
2026-04-09 18:19:06 -0600

    Initial Commit

---
+++ ccc/cccApp.swift
@@ -0,0 +1,17 @@
+//
+//  cccApp.swift
+//  ccc
+//
+//  Created by Vova Nguyen on 2026-04-09.
+//
+
+import SwiftUI
+
+@main
+struct cccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cccTests/cccTests.swift
@@ -0,0 +1,16 @@
+//
+//  cccTests.swift
+//  cccTests
+//
+//  Created by Vova Nguyen on 2026-04-09.
+//
+
+import Testing
+
+struct cccTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
